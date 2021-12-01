24a52ac0156d51afb652e9f01bc63b94e3e7ea09
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2021-12-01 15:07:06 +0100

    Day 01: Add solution for day one challenge

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -2,20 +2,22 @@ package day01
 
 import readInput
 
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+const val SLIDING_WINDOW_LENGTH = 3
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+fun main() {
+
+    fun mapToInts(input: List<String>) = input.map { it.toInt() }
+
+    fun part1(input: List<Int>) = input.zipWithNext().count { (previousNumber, nextNumber) -> previousNumber < nextNumber }
+
+    fun part2(input: List<Int>) = part1(input.windowed(SLIDING_WINDOW_LENGTH).map { it.sum() })
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("day01/Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = mapToInts(readInput("day01/Day01_test"))
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("day01/Day01")
+    val input = mapToInts(readInput("day01/Day01"))
     println(part1(input))
     println(part2(input))
 }
